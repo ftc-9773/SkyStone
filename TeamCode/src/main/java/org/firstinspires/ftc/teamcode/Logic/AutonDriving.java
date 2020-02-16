27e99f5b0e255e548c747493ad52d3c3b2aab2b4
Cadence Weddle <cadenceweddle@gmail.com>
2020-02-16 17:44:43 -0500

    calculated a bunch of constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Logic/AutonDriving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Logic/AutonDriving.java
@@ -23,17 +23,15 @@ public class AutonDriving {
     SafeJsonReader reader;
     MecanumDrivebase drivebase;
     LinearOpMode opMode;
-    double voltage;
-    double lastVoltageSampleTime = -1000; //We don't want to sample voltage that frequently because it changes slowly, and is expensive.
 
     static class consts{
         static double m; //Robots mass, kg
-        static double ke; //Constant of proportionality between current and torque of a motor
-        static double kv; //Constant of proportionality between wheel speed (rad/s) and back emf of a motor
-        static double r;//Radius of the robot's wheels, (m)
-        static double R;//Resistance of the motor + resistance of the battery
-        static double I;//rotational intertia of the wheels
-
+        static double kv = 12 / 628.3185307179587; //Constant of proportionality between wheel speed and back emf of a motor (Battery voltage / free speed). From https://www.revrobotics.com/rev-41-1301/: 6000rpm is converted to 628.3185307179587 rad/s.
+        static double ke = .105 / 8.5; //Constant of proportionality between torque and current. Calculated from (stall torque / stall current). Again, values taken from https://www.revrobotics.com/rev-41-1301/
+        static double r = 0.09975; //Radius of the robot's wheels, (m)
+        static double R = 0.5;//Resistance of the motor (ohms) (asked, Sanker, he said to assume 0.5 because measuring it is difficult. If the system screws up this may need to change)
+        static double I = 0.00017647166020;//rotational intertia of the wheels ((kg) * m^2). Wheels were modelled as a thick-walled cylinder. Calculation from: https://en.wikipedia.org/wiki/List_of_moments_of_inertia
+        static double voltage = 12; //Motor voltage, from https://www.revrobotics.com/rev-41-1301/
     }
 
     public AutonDriving(MecanumDrivebase drivebase, LinearOpMode opMode){
@@ -41,7 +39,6 @@ public class AutonDriving {
         this.drivebase = drivebase;
         this.opMode = opMode;
         readMatrix();
-        voltage = getBatteryVoltage();
     }
 
     //To make the matrix reader look nicer. Letter g chosen pseudorandomly.
@@ -61,10 +58,9 @@ public class AutonDriving {
     public void setCorrection(Matrix state, Matrix ref){
         double[] torques = K.times(ref.minus(state)).getArrayCopy()[0];
         double[] wheelVels = drivebase.wheelRotVels;
-        getBatteryVoltage(); //Call once, so that the value of voltage used is the same for all wheels, even if it is a little off.
         for (int i = 0; i < 4; i++) {
             torques[i] = torqueToVoltage(i, wheelVels[i]); //reuse the same array cause why not
-            torques[i] = torques[i] / voltage; //Percentage of available battery voltage to apply to the motor, cause thats how the motors work.
+            torques[i] = torques[i] / consts.voltage; //Percentage of available battery voltage to apply to the motor, cause thats how the motors work.
             torques[i] = bound(-1,1, torques[i]); //Power is on [-1, 1]. This call is for safety, ideally it does nothing.
         }
 
@@ -82,21 +78,5 @@ public class AutonDriving {
 
     public double torqueToVoltage(double torque, double wheel_velocity){
         return torque * consts.R / consts.ke - consts.kv * wheel_velocity;
-    }
-
-    double getBatteryVoltage() {
-        if (System.currentTimeMillis() - lastVoltageSampleTime < 1000){
-            return voltage;
-        }
-        double result = Double.POSITIVE_INFINITY;
-        for (VoltageSensor sensor : opMode.hardwareMap.voltageSensor) {
-            double voltage = sensor.getVoltage();
-            if (voltage > 0) {
-                result = Math.min(result, voltage);
-            }
-        }
-        voltage = result;
-        lastVoltageSampleTime = System.currentTimeMillis();
-        return result;
     }
 }
